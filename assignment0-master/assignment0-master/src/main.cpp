2d5e2603b1ea7e1bd2ace51e2af711fe05754288
Hooman-me <hanifashari03@gmail.com>
2026-03-13 17:35:53 +0700

    tugas01

--- assignment0-master/assignment0-master/src/main.cpp
+++ assignment0-master/assignment0-master/src/main.cpp
@@ -1,9 +1,14 @@
 #include <iostream>
+#include <string>
 
 using namespace std;
 
 int main(int argc, char ** argv) {
-    cout << "Hello world!" << endl;
+    string nama ;
+
+    cout << "Silahkan inputkan nama anda: ";
+    getline(cin, nama);
+    cout << "Hello " <<nama  << endl;
 
     return 0;
 }
